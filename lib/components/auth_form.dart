8a155934579c9a6d3c4d2cedd3e4c19369a6ce6d
navas <gabrielnavas@email.com>
2024-03-09 17:21:44 -0300

    refactory to method init animation

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -33,6 +33,10 @@ class _AuthFormState extends State<AuthForm>
   @override
   void initState() {
     super.initState();
+    _initAnimationController();
+  }
+
+  void _initAnimationController() {
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(
