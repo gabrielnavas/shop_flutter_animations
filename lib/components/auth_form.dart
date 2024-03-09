d1dba95d8de8a32a339e5859dea1fe3d2b4a03ab
navas <gabrielnavas@email.com>
2024-03-09 17:14:58 -0300

    add AnimatedBuilder and remove addListener

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -49,8 +49,6 @@ class _AuthFormState extends State<AuthForm>
         curve: Curves.linear,
       ),
     );
-
-    _heighAnimation?.addListener(() => setState(() {}));
   }
 
   @override
@@ -83,98 +81,103 @@ class _AuthFormState extends State<AuthForm>
     return Card(
       elevation: 8,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Container(
-        // height: _isSignup() ? 405 : 320,
-        height: _heighAnimation?.value.height ?? (_isSignup() ? 405 : 320),
-        width: deviceSize.width * 0.75,
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 10,
-        ),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) => AuthFormData.validateEmail(value ?? ''),
-                onChanged: (value) => _verifyForm(),
-                onSaved: (value) {
-                  if (value != null && value.isNotEmpty) {
-                    authData.email = value;
-                  }
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Senha'),
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                controller: _passwordController,
-                validator: (password) => _isLogin()
-                    ? AuthFormData.validatePassword(password ?? '')
-                    : AuthFormData.validatePasswords(
-                        password ?? '', _passwordConfirmationController.text),
-                onChanged: (value) => _verifyForm(),
-                onSaved: (value) {
-                  if (value != null && value.isNotEmpty) {
-                    authData.password = value;
-                  }
-                },
-              ),
-              if (_isSignup())
-                TextFormField(
-                  decoration:
-                      const InputDecoration(labelText: 'Confirmar senha'),
-                  keyboardType: TextInputType.text,
-                  controller: _passwordConfirmationController,
-                  obscureText: true,
-                  validator: (value) => AuthFormData.validatePasswords(
-                      value ?? '', _passwordController.text),
-                  onChanged: (value) => _verifyForm(),
-                  onSaved: (value) {
-                    if (value != null && value.isNotEmpty) {
-                      authData.passwordConfirmation = value;
-                    }
-                  },
+      child: AnimatedBuilder(
+          animation: _heighAnimation!,
+          builder: (context, child) => Container(
+                // height: _isSignup() ? 405 : 320,
+                height:
+                    _heighAnimation?.value.height ?? (_isSignup() ? 405 : 320),
+                width: deviceSize.width * 0.75,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
                 ),
-              const SizedBox(
-                height: 20,
-              ),
-              _isLoading
-                  ? circularProgress
-                  : ElevatedButton(
-                      onPressed: _submit,
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        backgroundColor: const Color.fromARGB(142, 18, 77, 178),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 30, vertical: 8),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: 'E-mail'),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) =>
+                            AuthFormData.validateEmail(value ?? ''),
+                        onChanged: (value) => _verifyForm(),
+                        onSaved: (value) {
+                          if (value != null && value.isNotEmpty) {
+                            authData.email = value;
+                          }
+                        },
                       ),
-                      child: Text(
-                        _isLogin() ? 'ENTRAR' : 'REGISTRAR',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(179, 255, 255, 255),
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: 'Senha'),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        controller: _passwordController,
+                        validator: (password) => _isLogin()
+                            ? AuthFormData.validatePassword(password ?? '')
+                            : AuthFormData.validatePasswords(password ?? '',
+                                _passwordConfirmationController.text),
+                        onChanged: (value) => _verifyForm(),
+                        onSaved: (value) {
+                          if (value != null && value.isNotEmpty) {
+                            authData.password = value;
+                          }
+                        },
+                      ),
+                      if (_isSignup())
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              labelText: 'Confirmar senha'),
+                          keyboardType: TextInputType.text,
+                          controller: _passwordConfirmationController,
+                          obscureText: true,
+                          validator: (value) => AuthFormData.validatePasswords(
+                              value ?? '', _passwordController.text),
+                          onChanged: (value) => _verifyForm(),
+                          onSaved: (value) {
+                            if (value != null && value.isNotEmpty) {
+                              authData.passwordConfirmation = value;
+                            }
+                          },
                         ),
-                      )),
-              const Spacer(),
-              TextButton(
-                onPressed: _switchAuthMode,
-                child: Text(
-                  _isLogin() ? 'DESEJA REGISTRAR?' : 'JÁ POSSUI CONTA?',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(201, 18, 77, 178),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      _isLoading
+                          ? circularProgress
+                          : ElevatedButton(
+                              onPressed: _submit,
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                backgroundColor:
+                                    const Color.fromARGB(142, 18, 77, 178),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 30, vertical: 8),
+                              ),
+                              child: Text(
+                                _isLogin() ? 'ENTRAR' : 'REGISTRAR',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(179, 255, 255, 255),
+                                ),
+                              )),
+                      const Spacer(),
+                      TextButton(
+                        onPressed: _switchAuthMode,
+                        child: Text(
+                          _isLogin() ? 'DESEJA REGISTRAR?' : 'JÁ POSSUI CONTA?',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(201, 18, 77, 178),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-              )
-            ],
-          ),
-        ),
-      ),
+              )),
     );
   }
 
