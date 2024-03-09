f7d246c8514dbbfc164de0fe92fc31bece23a5a4
navas <gabrielnavas@email.com>
2024-03-09 19:17:25 -0300

    refactory with animared container

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -16,81 +16,98 @@ class _OrderWidgetState extends State<OrderWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('R\$${widget.order.total.toStringAsFixed(2)}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
-            ),
-            trailing: IconButton(
-              icon: const Icon(Icons.expand_more),
-              onPressed: () => setState(() {
-                _expanded = !_expanded;
-              }),
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: (widget.order.cartItems.length * 32) + 5,
-              child: ListView(
-                  children: widget.order.cartItems
-                      .map(
-                        (product) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  product.productName,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text('${product.quantity}x',
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.normal,
-                                      color: Colors.black54,
-                                    )),
-                                const SizedBox(
-                                  width: 10,
-                                ),
-                                Text(
-                                  'R\$${product.productPrice.toStringAsFixed(2)}',
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.normal,
-                                    color: Colors.black54,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 10,
-                                ),
-                                Text(
-                                  'R\$${(product.productPrice * product.quantity).toStringAsFixed(2)}',
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.black87,
-                                  ),
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                      )
-                      .toList()),
-            )
-        ],
+    final double itemsHeight = (widget.order.cartItems.length * 32) + 5;
+    Duration durationAnimated = const Duration(milliseconds: 280);
+
+    Widget order = _renderOrder();
+    Widget orderItems = renderOrderItems(durationAnimated, itemsHeight);
+
+    return AnimatedContainer(
+      duration: durationAnimated,
+      height: _expanded ? itemsHeight + 95 : 95,
+      child: Card(
+        child: Column(
+          children: [order, orderItems],
+        ),
       ),
     );
+  }
+
+  Widget renderOrderItems(Duration durationAnimated, double itemsHeight) {
+    Widget orderItems = AnimatedContainer(
+      duration: durationAnimated,
+      height: _expanded ? itemsHeight : 0,
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      child: ListView(
+          children: widget.order.cartItems
+              .map(
+                (product) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          product.productName,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('${product.quantity}x',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black54,
+                            )),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'R\$${product.productPrice.toStringAsFixed(2)}',
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'R\$${(product.productPrice * product.quantity).toStringAsFixed(2)}',
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black87,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+              .toList()),
+    );
+    return orderItems;
+  }
+
+  Widget _renderOrder() {
+    Widget order = ListTile(
+      title: Text('R\$${widget.order.total.toStringAsFixed(2)}'),
+      subtitle: Text(
+        DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
+      ),
+      trailing: IconButton(
+        icon: const Icon(Icons.expand_more),
+        onPressed: () => setState(() {
+          _expanded = !_expanded;
+        }),
+      ),
+    );
+    return order;
   }
 }
